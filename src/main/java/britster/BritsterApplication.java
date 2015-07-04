2c7f24cff889972e7c8d1c6747afd87806d48523
Jeffrey Michel <jemichel@yahoo.com>
2015-07-04 12:33:01 -0400

    add web request mapping and ignore build/

--- src/main/java/britster/BritsterApplication.java
+++ src/main/java/britster/BritsterApplication.java
@@ -2,9 +2,16 @@ package britster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class BritsterApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BritsterApplication.class, args);
