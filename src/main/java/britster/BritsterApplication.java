c9a3b23a19f7c9283b5a7f5245c7ba252e7b2454
Jeffrey Michel <jemichel@yahoo.com>
2015-07-04 12:55:30 -0400

    add Controller annotation to make GET / work

--- src/main/java/britster/BritsterApplication.java
+++ src/main/java/britster/BritsterApplication.java
@@ -2,8 +2,10 @@ package britster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+@Controller
 @SpringBootApplication
 public class BritsterApplication {
 
