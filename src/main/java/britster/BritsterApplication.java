1103018131b9f97ca460279162c1cf8533570c4f
Jeffrey Michel <jemichel@yahoo.com>
2015-07-04 11:54:26 -0400

    initial commit

---
+++ src/main/java/britster/BritsterApplication.java
@@ -0,0 +1,12 @@
+package britster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BritsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BritsterApplication.class, args);
+    }
+}
